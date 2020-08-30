ca7dd5b2b06e548059b2f101940bdd63ed2e7601
Popov Ivan (dev777popov) <i.popov@start.ru>
2020-08-30 23:24:02 +0300

    finish homework1

--- app/src/main/java/com/dev777popov/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/dev777popov/kotlinproject/MainActivity.kt
@@ -2,10 +2,20 @@ package com.dev777popov.kotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        var count = 0
+        btnAdd.setOnClickListener {
+            count++
+            txtValue.text = count.toString()
+        }
     }
 }
